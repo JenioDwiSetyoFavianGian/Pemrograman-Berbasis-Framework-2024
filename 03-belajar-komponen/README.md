487f820532e69accfe1cfd3572a6701bf3ab03aa
JenioDwiSetyoFavianGian <jeniodwisetyofaviang@gmail.com>
2024-02-29 02:34:19 +0700

    W03: Jawaban soal 3

--- 03-belajar-komponen/README.md
+++ 03-belajar-komponen/README.md
@@ -6,6 +6,9 @@ NIM     : 2141720212
 ## Output Soal 1
 ![Screenshoot](docs/soal1.png) 
 Mengubah code pada file 'page.tsx'
-## Output Soal 1
+## Output Soal 2
 ![Screenshoot](docs/soal2.png) 
 Dengan menambahkan kode pada file page.tsx yang berfungsi untuk memanggil komponen Gallery
+## Output Soal 3
+![Screenshoot](docs/soal3.png) 
+Memastikan bahwa semua elemen JSX berada dalam satu elemen induk, dalam hal ini 'div', mengganti penggunaan class dengan className, mengganti br menjadi br /, penutup tag yang benar dalam JSX, dan merapikan penggunaan tag b dan i.
--- 03-belajar-komponen/src/app/page.tsx
+++ 03-belajar-komponen/src/app/page.tsx
@@ -2,18 +2,31 @@ import Image from "next/image";
 import Profile from "./components/profile";
 import { Gallery } from "./components/gallery";
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main className="flex flex-col items-center">
+//       <p className="justify-center mt-16">Ilmuwan yang luar biasa </p>
+//       <div className="flex mt-16">
+//           <Profile />
+//           <div className="mx-96">    
+//           <Profile/>   
+//           </div> 
+//           <Profile/>
+//       </div>
+//       {<Gallery />}
+//     </main>
+//   );
+// }
+
+export default function Bio() {
   return (
-    <main className="flex flex-col items-center">
-      <p className="justify-center mt-16">Ilmuwan yang luar biasa </p>
-      <div className="flex mt-16">
-          <Profile />
-          <div className="mx-96">    
-          <Profile/>   
-          </div> 
-          <Profile/>
-      </div>
-      {<Gallery />}
-    </main>
+    <div className="intro">
+      <h1>Selamat datang di website saya!</h1>
+      <p className="summary">
+        Anda dapat membaca uneg-unegku di sini.
+        <br /><br />
+        <b>Juga ada <i>foto</i> ilmuwan!</b>
+      </p>
+    </div>
   );
 }
